7b946e46e33e8a9413e271a829fc9b8fe98a13ad
albinjose <albinavirappatt@gmail.com>
2020-03-09 12:32:57 +0530

    create server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express()
+
+// Home Route
+app.get('/', (req, res) =>{
+    res.send("Server working properly");
+})
+
+app.listen(8000, () => {
+    console.log("Server started running on Port 8000");
+})
